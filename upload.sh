d2b71c2ec1ddcc01599f9a5a8a88e4fd3d76d2d6
koko <ko282ko82@gmail.com>
2024-10-06 20:40:40 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+DIRROM="Pixel14/"
+# Check if the directory exists
+if [ -d "$DIRROM" ]; then
+    echo "Directory $DIRROM exists. Deleting it..."
+    crave clone destroy "$DIRROM" -y
+    echo "Directory deleted."
+else
+    echo "Directory $DIRROM does not exist. No need to delete."
+fi
+
 #Create Project Dir
 crave clone create --projectID 82 /crave-devspaces/Pixel14
 
-echo "========================================================================"
 echo "Project dir Created"
 echo "========================================================================"
 
 #Go into the dir
 cd Pixel14
 
-echo "========================================================================"
 echo "Inside the Project dir"
 echo "========================================================================"
 
@@ -19,19 +27,16 @@ rm .repo/manifests/crave.yaml* || true; # Removes existing crave.yamls
 
 curl -o .repo/manifests/crave.yaml https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/main/configs/crave/crave.yaml.aosp # Downloads crave.yaml
 
-echo "========================================================================"
 echo "cloned crave yaml"
 echo "========================================================================"
 
 crave pull out/target/product/a52q/PixelOS_a52q-14.0-20241006-0548.zip
 
-echo "========================================================================"
 echo "pulled rom zip"
 echo "========================================================================"
 
 cd /home/crave-devspaces
 
-echo "========================================================================"
 echo "home dir"
 echo "========================================================================"
 
