409975d778c448165960b1363ba2e2ca7402f3cd
koko <ko282ko82@gmail.com>
2024-10-06 22:04:58 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
-rm -rf device/realme
-rm -rf kernel/oplus
-rm -rf vendor/realme
-rm -rf hardware/oplus
-rm -rf device/oneplus
-rm -rf vendor/oneplus
-rm -rf vendor/oplus
-
-# Cleanup to fix SyncErrors raised during branch checkouts
-rm -rf platform/prebuilts
+rm -rf device/samsung
+rm -rf kernel/samsung
+rm -rf vendor/samsung
+rm -rf hardware/samsung
+rm -rf vendor/extra
+rm -rf packages/apps/Updates
 
 echo "========================================================================"
 echo "DELETED DIRECTORIES"
 echo "========================================================================"
 
 
-# Repo Init
-repo init -u https://github.com/ProjectBlaze/manifest -b 15 --git-lfs --depth=1
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
 
 echo "========================================================================"
-echo "REPO INITIALIZED"
+echo "REPO INITIALISED"
 echo "========================================================================"
 
 
 # Clone local_manifests repository
-git clone https://github.com/DevInfinix/android-aosp-local-manifests --depth 1 -b 15-blaze .repo/local_manifests
+git clone https://github.com/koko-07870/local_manifests --depth 1 -b pos .repo/local_manifests
 if [ ! 0 == 0 ]
-    then curl -o .repo/local_manifests https://github.com/DevInfinix/android-aosp-local-manifests.git
+    then curl -o .repo/local_manifests https://github.com/koko-07870/local_manifests.git
 fi
 
 echo "========================================================================"
-echo "CLONED REPOS"
+echo "CLONED local manifest"
 echo "========================================================================"
 
 
 # Resync
-
 /opt/crave/resync.sh
 
 echo "========================================================================"
@@ -45,29 +39,17 @@ echo "RESYNCED"
 echo "========================================================================"
 
 
-# Upgrade System and install openssl
+# Upgrade System
 
 sudo apt update && sudo apt upgrade -y
 
-echo "========================================================================"
+
 echo "SYSTEM UPGRADED"
 echo "========================================================================"
 
 
-# Clone Custom Clang
-
-CUSTOMCLANG="azure"
-rm -rf "prebuilts/clang/host/linux-x86/clang-${CUSTOMCLANG}"
-git clone https://gitlab.com/Panchajanya1999/azure-clang --depth=1 -b main prebuilts/clang/host/linux-x86/clang-${CUSTOMCLANG}
-
-echo "========================================================================"
-echo "CLONED CUSTOM CLANG"
-echo "========================================================================"
-
-
 # Clone Keys
-
-DIRKEYS="vendor/blaze/signing/keys/"
+DIRKEYS="vendor/aosp/signing/keys"
 # Check if the directory exists
 if [ -d "$DIRKEYS" ]; then
     echo "Directory $DIRKEYS exists. Deleting it..."
@@ -77,21 +59,51 @@ else
     echo "Directory $DIRKEYS does not exist. No need to delete."
 fi
 
-echo "Cloning the repository..."
-git clone https://github.com/DevInfinix/devinfinix-aosp-roms-keys --depth=1 -b 15.0-blaze "$DIRKEYS"
+#get keys
+wget https://github.com/koko-07870/scripts/blob/new/keys.zip && unzip -o keys.zip -d "$DIRKEYS" && rm keys.zip
 
-echo "========================================================================"
 echo "CLONED KEYS"
 echo "========================================================================"
 
 
+OTA1="vendor/aosp/build/core/main_version.mk"
+# Check if exists
+if [ -x "$OTA1" ]; then
+    echo "File $OTA1 exists. Deleting it..."
+    rm -rf "OTA1"
+    echo "main_version.mk deleted."
+else
+    echo "Directory $OTA1 does not exist. No need to delete."
+fi
+
+#get main_version.mk
+wget https://raw.githubusercontent.com/koko-07870/scripts/refs/heads/new/main_version.mk -P vendor/aosp/build/core/
+
+echo "OTA1 CLONED"
 echo "========================================================================"
-echo "BUILDING........."
+
+
+OTA2="vendor/aosp/config/ota.mk"
+# Check if exists
+if [ -x "$OTA2" ]; then
+    echo "File $OTA2 exists. Deleting it..."
+    rm -rf "OTA2"
+    echo "ota.mk deleted."
+else
+    echo "Directory $OTA2 does not exist. No need to delete."
+fi
+
+#get ota.mk
+wget https://github.com/koko-07870/scripts/blob/new/ota.mk -P vendor/aosp/config/
+
+echo "ota.mk cloned"
 echo "========================================================================"
 
 
 # Lunch
+export BUILD_USERNAME=koko-07870 
+export BUILD_HOSTNAME=crave
 source build/envsetup.sh
-lunch blaze_ice-ap3a-userdebug
+lunch aosp_a52q-ap2a-userdebug
 make installclean
-make bacon
+mka bacon
